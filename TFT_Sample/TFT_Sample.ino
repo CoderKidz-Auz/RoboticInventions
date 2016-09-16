f234fabd495d77f738a7bc9d644a3e743f7c3eb1
alex.jacobs@outlook.com.au <alex.jacobs@outlook.com.au>
2016-09-16 21:07:45 +1000

    New Tutorials added.

---
+++ TFT_Sample/TFT_Sample.ino
@@ -0,0 +1,66 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <TouchScreen.h>
+
+#if defined(__SAM3X8E__)
+    #undef __FlashStringHelper::F(string_literal)
+    #define F(string_literal) string_literal
+#endif
+
+// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// For the Arduino Uno, Duemilanove, Diecimila, etc.:
+//   D0 connects to digital pin 8  (Notice these are
+//   D1 connects to digital pin 9   NOT in order!)
+//   D2 connects to digital pin 2
+//   D3 connects to digital pin 3
+//   D4 connects to digital pin 4
+//   D5 connects to digital pin 5
+//   D6 connects to digital pin 6
+//   D7 connects to digital pin 7
+//   LCD_CS connects to A3
+//   LCD_CD connects to A2
+//   LCD_WR connects to A1
+//   LCD_RD connects to A0
+//   LCD_RESET connects to A4
+
+#define LCD_CS A3
+#define LCD_CD A2
+#define LCD_WR A1
+#define LCD_RD A0
+// optional
+#define LCD_RESET A4
+
+// Assign human-readable names to some common 16-bit color values:
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+char dataRx;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println(F("Paint!"));
+  
+  tft.reset();
+  uint16_t identifier = 0x9341;
+  tft.begin(identifier);
+  tft.fillScreen(BLACK);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  while(!Serial.available());
+  dataRx = Serial.read();
+  tft.setTextColor(YELLOW);
+  tft.setTextSize(3);
+  tft.print(dataRx);
+}
+
